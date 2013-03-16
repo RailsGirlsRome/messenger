442e6d6792a72a1cc34f4af6e5e35482be1fa9cb
Fabrizio Regini <freegenie@gmail.com>
2013-03-17 00:24:33 +0100

    use select to pick records

--- app/models/message_delivery.rb
+++ app/models/message_delivery.rb
@@ -1,5 +1,7 @@
 class MessageDelivery < ActiveRecord::Base
   belongs_to :audio_file
   belongs_to :contact
-  # attr_accessible :title, :body
+  attr_accessible :audio_file_id, :contact_id
+
+  validates_presence_of :audio_file_id, :contact_id
 end
