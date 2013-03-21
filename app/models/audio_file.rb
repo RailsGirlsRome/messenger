1b9c072db27e932aaceb75d6a46d1516be3a2454
Fabrizio Regini <freegenie@gmail.com>
2013-03-21 02:05:46 +0100

    AudioFile: upload file to s3

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -1,5 +1,8 @@
 class AudioFile < ActiveRecord::Base
   attr_accessible :name, :file
 
-  has_attached_file :file
+  has_attached_file :file,
+    storage: :s3,
+    s3_credentials: "#{Rails.root.join('config', 's3_credentials.yml')}"
+
 end
